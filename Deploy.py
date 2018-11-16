469ea2722eb7bd2ca83d6d44d5797e84c9b20415
tilottama90 <45002505+tilottama90@users.noreply.github.com>
2018-11-16 13:58:04 +0530

    Update Deploy.py

--- Deploy.py
+++ Deploy.py
@@ -4,4 +4,4 @@ url='t3://localhost:7101'
 
 connect(user, password, url)
 
-sca_deployComposite("http://localhost:7101", "sca_CELFServiceWrapper.jar", user='weblogic', password='webl0gic', partition='default')
+sca_deployComposite("http://localhost:7101", "sca_my-hello-world-soa-project_rev1.0.jar", user='weblogic', password='webl0gic', partition='default')
